c798997a0dd546f8b027247de68dc51d5dea87b9
prsndp <prasandeepp@gmail.com>
2022-12-12 16:55:40 +0530

    server creaated!

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from  'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+const app = express()
+dotenv.config()
+
+const connect = () =>{
+    mongoose
+    .connect(process.env.MONGO)
+        .then(()=>{
+            console.log('Connected to DB');
+        })
+        .catch(err =>{
+        throw err;
+    })
+}
+
+
+app.listen(8800, ()=>{
+    connect()
+    console.log('Server Connected !')
+})
+
