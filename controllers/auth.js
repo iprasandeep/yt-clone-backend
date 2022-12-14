34695f352a1ff67ce663ba38be9a1cb5537136e0
prsndp <prasandeepp@gmail.com>
2022-12-14 14:59:33 +0530

    changes done in controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,10 @@
-import mongoose, { trusted } from 'mongoose';
+import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import { createError } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next)=>{
-
     console.log(req.body)
     try{
         const salt = bcrypt.genSaltSync(10);
@@ -15,28 +14,30 @@ export const signup = async (req, res, next)=>{
         await newUser.save();
         res.status(200).send('User has been created!')
     }catch(err){
-        next(createError(404, ' Sorry equest Not Found!'));
+        next(createError(404, ' Sorry request Not Found!'));
         // 
     }
 };
-
 // sign-in
 export const signin = async (req, res, next)=>{
-    try{
-        
-        const user = await User.findOne({name:req.body.name})
+    
+    try{  
+        const user = await User.findOne({name: req.body.name})
+        // console.log(req.body.name); 
+        // console.log(user);
         if(!user) return next(createError(404, 'User not found!'));
-
         const isCorrect = await bcrypt.compare(req.body.password, user.password)
-          
-        if(!isCorrect) 
-            return next(createError(400, 'Wrong credentials'));
+        // console.log(user.password);
+        if(!isCorrect) return next(createError(400, 'Wrong credentials'));
         
         const token = jwt.sign({id:user._id}, process.env.JWT);
-
-        res.cookies('access_token', token, {
+        const { password, ...others }  = user._doc;
+        res
+        .cookie('access_token', token, {
             httpOnly:true
-        }).status(200)
+        })
+        .status(200)
+        .json(others);
     }
     catch(err)
     {
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,61 @@
-export const test = (req, res)=>{
-    res.json('Its Running..!!')
-    console.log('Test is working!');
+import User from "../models/User.js";
+import { createError } from "../utils/error.js";
+
+export const update =  async (req, res, next)=>{
+    if(req.params.id ===  req.user.id){
+        // todo 
+    try{
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id, 
+            { 
+                $set: req.body, 
+            }, 
+            {
+                new: true
+            }
+        )
+        res.status(200).json(updateUser)
+
+    } catch(err){
+        next(err);
+    }
+
+    }
+    else{
+        return next(createError(403, 'You can update only your account!'));
+    }
 }
+export const deleteUser = async (req, res, next)=>{
+    if(req.params.id ===  req.user.id){
+        // todo 
+    try{
+        await User.findByIdAndDelete(
+            req.params.id, 
+          
+        )
+        res.status(200).json('User has been deleted');
+    } catch(err){
+        next(err);
+    }
+}
+    else{
+        return next(createError(403, 'You can delete only your account!'));
+    }
+    
+}
+export const getUser = (req, res, next)=>{
+    
+}
+export const subscribe = (req, res, next)=>{
+    
+}
+export const unsubscribe = (req, res, next)=>{
+    
+}
+export const like = (req, res, next)=>{
+    
+}
+export const dislike = (req, res, next)=>{
+    
+}
+
